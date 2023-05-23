f4b355edcdbd695f23b719782f11427afd973e41
윤태준 <ta1231@ajou.ac.kr>
2023-05-23 13:09:35 +0900

    pred3수정

--- main.py
+++ main.py
@@ -108,8 +108,8 @@ async def predict(request: Request, input_data: InputData):
 async def predict(request: Request, input_data: InputData):
     # 문자열 리스트를 파이썬 리스트로 변환
     input_tensor = np.array(input_data.data, dtype=float)
-    prediction = model(input_tensor.reshape(-1, 50, 6))
-    # prediction = model(scaler.transform(input_tensor).reshape(-1, 50, 6))
+    # prediction = model(input_tensor.reshape(-1, 50, 6))
+    prediction = model(scaler.transform(input_tensor).reshape(-1, 50, 6))
 
     # 각 행에서 가장 큰 값을 가지는 열의 인덱스를 찾음
     max_index = np.argmax(prediction, axis=1)
